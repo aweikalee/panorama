bae764c49e1a7fb37112bc730e111f44a6eeb59c
aweikalee <aweikalee@163.com>
2025-10-03 15:38:21 +0800

    feat: 图片查看

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import "normalize.css"
 import "./style.css"
 import App from "./App.vue"
 import router from "./router"
+import EasyLightBox from "vue-easy-lightbox"
 
 const app = createApp(App)
 app.use(router)
+app.use(EasyLightBox)
 app.mount("#app")
--- src/pages/gallery/index.vue
+++ src/pages/gallery/index.vue
@@ -2,29 +2,57 @@
 import Navbar from "@/components/Navbar.vue"
 import Fixed from "@/components/Fixed.vue"
 import { getAssets } from "@/utils/oss"
+import { useEasyLightbox } from "vue-easy-lightbox"
 
-const list = [
+const list: { name: string; url: string; width?: number; height?: number }[] = [
   {
     name: "平面图",
     url: getAssets("主方案_2025-10-02T06_42_20.7966195Z.jpg"),
   },
   {
     name: "顶面示意图",
-    url: getAssets("主方案_2025-10-02T06_42_20.7966195Z.jpg"),
+    url: getAssets("主方案_2025-10-02T06_51_22.0568531Z.jpg"),
   },
 ]
+
+const { imgsRef, indexRef, onHide, visibleRef } = useEasyLightbox()
 </script>
 
 <template>
   <div class="gallery">
-    <div class="gallery__item" v-for="item in list" :key="item.name">
+    <a
+      class="gallery__item"
+      v-for="item in list"
+      :key="item.name"
+      target="_blank"
+      @click="
+        () => {
+          visibleRef = true
+          indexRef = list.indexOf(item)
+          imgsRef = list.map((item) => item.url)
+        }
+      "
+    >
       {{ item.name }}
-    </div>
+    </a>
   </div>
 
   <Fixed position="bottom">
     <Navbar />
   </Fixed>
+
+  <vue-easy-lightbox
+    :visible="visibleRef"
+    :imgs="imgsRef"
+    :index="indexRef"
+    @hide="onHide"
+    :zoomScale="1"
+    style="background: rgba(0, 0, 0, 0.8)"
+  >
+    <template #prev-btn> </template>
+    <template #next-btn> </template>
+    <template #toolbar> </template
+  ></vue-easy-lightbox>
 </template>
 
 <style scoped lang="scss">
