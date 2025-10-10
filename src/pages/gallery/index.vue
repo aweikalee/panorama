5a0e2c4379ae5a4f7c41fbb3ad8bc09ba157bb8a
aweikalee <aweikalee@163.com>
2025-10-10 16:40:50 +0800

    feat: 平面图 数据更新

--- src/pages/gallery/index.vue
+++ src/pages/gallery/index.vue
@@ -6,35 +6,43 @@ import { getAssets } from "@/utils/oss"
 const list: { name: string; url: string; width?: number; height?: number }[] = [
   {
     name: "平面图",
-    url: getAssets("平面图/平面图_1_平面图.png"),
+    url: getAssets("平面图/平面图_01_平面图.png"),
   },
   {
     name: "墙体拆改指示图",
-    url: getAssets("平面图/平面图_2_墙体拆改指示图.png"),
+    url: getAssets("平面图/平面图_02_墙体拆改指示图.png"),
   },
   {
     name: "吊顶示意图",
-    url: getAssets("平面图/平面图_3_吊顶示意图.png"),
+    url: getAssets("平面图/平面图_03_吊顶示意图.png"),
   },
   {
     name: "开关插座布置图",
-    url: getAssets("平面图/平面图_4_开关插座布置图.png"),
+    url: getAssets("平面图/平面图_04_开关插座布置图.png"),
   },
   {
     name: "灯控图",
-    url: getAssets("平面图/平面图_5_灯控图.png"),
+    url: getAssets("平面图/平面图_05_灯控图.png"),
+  },
+  {
+    name: "给水平面图",
+    url: getAssets("平面图/平面图_06_给水平面图.png"),
+  },
+  {
+    name: "新风平面图",
+    url: getAssets("平面图/平面图_07_新风平面图.png"),
   },
   {
     name: "水电立面图1",
-    url: getAssets("平面图/平面图_6_水电立面图1.png"),
+    url: getAssets("平面图/平面图_08_水电立面图1.png"),
   },
   {
     name: "水电立面图2",
-    url: getAssets("平面图/平面图_7_水电立面图2.png"),
+    url: getAssets("平面图/平面图_09_水电立面图2.png"),
   },
   {
     name: "水电立面图3",
-    url: getAssets("平面图/平面图_8_水电立面图3.png"),
+    url: getAssets("平面图/平面图_10_水电立面图3.png"),
   },
 ]
 </script>
